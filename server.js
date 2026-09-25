c34eeed085ebef6e811c486949113e0495c4e144
sammy <142742278+sammy-g-o@users.noreply.github.com>
2026-09-25 21:54:25 +0100

    feat: add database connection setup with mongoose and dotenv

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config({ path: "./config.env" });
 import { app } from "./app.js";
+
+const DB = process.env.ATLAS_STRING.replace(
+  "<db_password>",
+  process.env.MONGODB_PASSWORD,
+);
+
+mongoose.connect(DB).then(() => console.log(`Database connected successfully`));
 
 const PORT = 3000;
 
